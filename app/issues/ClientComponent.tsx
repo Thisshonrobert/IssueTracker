924db478e4134cc7ad214d6ed4723645e72b4d85
Thisshonrobert <thisshonrobert0205@gmail.com>
2024-07-22 22:49:54 +0530

    bug fixed

--- app/issues/ClientComponent.tsx
+++ app/issues/ClientComponent.tsx
@@ -24,7 +24,7 @@ const ClientComponent = () => {
           <p className={`fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 ${isDarkTheme ? '' : ''}`}>
           Manage and Track Issues Seamlessly
           </p>
-          <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+          <div className="fixed bottom-0 left-0 flex w-full items-end rounded md:rounded-xl justify-center border  border-gray-300 bg-gradient-to-t p-2 from-zinc-200 dark:border-neutral-800 dark:bg-zinc-800/30  lg:static lg:size-auto lg:bg-none ">
             <a
               className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
               href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
@@ -37,7 +37,7 @@ const ClientComponent = () => {
                 alt="Vercel Logo"
                 className={isDarkTheme ? "dark:invert" : ""}
                 width={100}
-                height={24}
+                height={28}
                 priority
               />
             </a>
